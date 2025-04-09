1a045a5efc105e1c025ee452765642f20d82bc1c
Italo Marcelo <devfrontend2028study@gmail.com>
2025-04-09 15:56:22 -0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,10 @@ function App() {
 
 
   return (
-    // <Header/>
+   <>
+    <Header/>
     <OurCollection/>
+    </>
   )
 }
 
--- src/pages/OurCollection.jsx
+++ src/pages/OurCollection.jsx
@@ -3,17 +3,22 @@ import CoffeeList from '../components/CoffeList';
 
 export default function OurCollection() {
     return (
-      <Stack component='section' position='relative'>
+      <Stack component='section' position='relative' sx={{display:'flex', flexDirection: {xs:'column', lg: 'row'},
+       justifyContent: {xs: 'center', lg: 'flex-start'},
+       alignItems: {xs: 'center', lg: 'flex-start'},
+       alignContent: {xs: 'center', lg: 'flex-start'},
+       textAlign: 'center'
+      }} padding={{xs: '1em 2em', lg: '1em 7rem'}}>
          <Stack className='collection-T'></Stack>
          {
            CoffeeList.map((data, index) => {
               return   <Stack key={index} className='Coffee-block'>
-              <Stack className='coffee-image'>
+              <Stack className='coffee-image' margin={{xs: '2em 0', lg: '1em 0'}}>
                   <img src={data.Icon} alt={data.Title}/>
               </Stack>
               <Stack className='info'>
                  <Typography variant='h2' fontSize='1.2em'>{data.Title}</Typography>
-                 <Typography variant='p'>{data.Text} </Typography>
+                 <Typography variant='p' margin={{xs: '1em 0', lg: '2em 0'}} >{data.Text} </Typography>
               </Stack>
            </Stack>
            })
