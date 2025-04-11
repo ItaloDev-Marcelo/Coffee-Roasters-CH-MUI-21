a3ae1c4f3460788125b343a88437e90b22ed7d0f
Italo Marcelo <devfrontend2028study@gmail.com>
2025-04-11 15:20:00 -0300

    up

--- src/components/whyChooseHeader.jsx
+++ src/components/whyChooseHeader.jsx
@@ -2,10 +2,16 @@ import { Stack, Typography } from '@mui/material';
 
 const  WhyChooseHeader = () => {
    return  (
-       <Stack component='section' id='WhyChooseUs-content'>
-                           <Typography variant='h2' >Why choose us?</Typography>
-                           <Typography variant='p'>A large part of our role is choosing which particular coffees will be featured in our range. This means working closely with the best coffee growers to give you a more impactful experience on every level.</Typography>
-                       </Stack>
+       <Stack component='section' id='WhyChooseUs-content'
+     
+       >
+       <Stack >
+       <Typography variant='h2' fontSize={{xs: '1.2em', lg: '1.5em'}} >Why choose us?</Typography>
+                           <Typography variant='p' >A large part of our role is choosing which particular coffees will be featured in our range. This means working 
+                            closely with the best coffee growers to give you a more impactful 
+                            experience on every level.</Typography>
+       </Stack>
+       </Stack>
    )
 }
 
--- src/pages/WhyChooseUs.jsx
+++ src/pages/WhyChooseUs.jsx
@@ -10,19 +10,24 @@ import WhyCard from '../components/WhyCards'
 export default function WhyChooseUs() {
     return (
         <Stack component='section' id='WhyChooseUs'>
-            <Box id='blockImage-Container'  display={{xs: 'block', sm: 'none'}} sx={{ position: 'relative' }}>
+            <Box className='blockImage-Container' 
+             display={{xs: 'block', sm: 'none'}} sx={{ position: 'relative'}}>
                 <img src={Block0} alt='' />
                 <WhyChooseHeader/>
                 <WhyCard/>
+               
             </Box>
 
-            <Box id='blockImage-Container' display={{xs:'none', sm: 'block', lg: 'none'}} sx={{ position: 'relative' }}>
+            <Box className='blockImage-Container'
+             display={{xs:'none', sm: 'block', lg: 'none'}} sx={{ position: 'relative' }}>
                 <img src={Block1} alt='' />
                 <WhyChooseHeader />
                 <WhyCard />
+                
             </Box>
 
-            <Box id='blockImage-Container' display={{xs:'none', lg: 'block'}} sx={{ position: 'relative' }}>
+            <Box className='blockImage-Container' 
+            display={{xs:'none', lg: 'block'}} sx={{ position: 'relative' }}>
                 <img src={Block2} alt='' />
                 <WhyChooseHeader />
                 <WhyCard />
