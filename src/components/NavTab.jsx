259fed25959f276d21eedb4452e9cca40d10e350
Italo Marcelo <devfrontend2028study@gmail.com>
2025-04-20 17:26:12 -0300

    up

--- src/components/NavTab.jsx
+++ src/components/NavTab.jsx
@@ -1,4 +1,5 @@
 import {Stack} from '@mui/material';
+import React, { useState } from 'react';
 import logo from '../assets/images/shared/desktop/logo.svg';
 import open from '../assets/images/shared/mobile/icon-hamburger.svg';
 import close from '../assets/images/shared/mobile/icon-close.svg';
@@ -7,17 +8,20 @@ export default function WhyCard() {
 
 
     const navegate = useNavigate()
+
+    const [menu, setMenu] = useState(false);
    
 
     return (
-         <Stack component='header'  >
+         <Stack component='header' padding='2em'  sx={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}} >
                     <Stack>
-                        <img src={logo} alt='logo' />
+                        <img src={logo} width='200px' alt='logo' />
                     </Stack>
                     <Stack display={{ xs: 'block', md: 'none' }} >
                         <img src={open} alt='open' />
                     </Stack>
-                 <nav>
+                <Stack onClick={() => setMenu(!menu)} className={menu ? ' menu active' : 'menu'}>
+                <nav>
                  <Stack display={{ xs: 'block', md: 'none' }} >
                         <img src={close} alt='close' />
                     </Stack>
@@ -27,6 +31,7 @@ export default function WhyCard() {
                         <li onClick={() => navegate('/Plan')}>Create your plan</li>
                     </ul>
                 </nav>
+                </Stack>
          </Stack>
     )
 }
