2d8348cad8f3d46d20605b65d808837be791b787
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-05-14 18:33:32 -0300

    new component

---
+++ src/components/ImagesBank.jsx
@@ -0,0 +1,62 @@
+import CoffeeImage0 from '../assets/images/home/desktop/image-gran-espresso.png';
+import CoffeeImage1 from '../assets/images/home/desktop/image-planalto.png';
+import CoffeeImage2 from '../assets/images/home/desktop/image-piccollo.png';
+import CoffeeImage3 from '../assets/images/home/desktop/image-danche.png';
+import LogoWhite from '../assets/images/shared/desktop/logo-white.svg';
+import Icon0 from '../assets/images/about/desktop/illustration-uk.svg';
+import Icon1 from '../assets/images/about/desktop/illustration-canada.svg';
+import Icon2 from '../assets/images/about/desktop/illustration-australia.svg';
+import Logo from '../assets/images/shared/desktop/logo.svg';
+import Open from '../assets/images/shared/mobile/icon-hamburger.svg';
+import Close from '../assets/images/shared/mobile/icon-close.svg';
+import Beans from '../assets/images/home/desktop/icon-coffee-bean.svg';
+import Gift from '../assets/images/home/desktop/icon-gift.svg';
+import Truck from '../assets/images/home/desktop/icon-truck.svg';
+import Barista0 from '../assets/images/about/mobile/image-commitment.jpg';
+import Barista1 from '../assets/images/about/tablet/image-commitment.jpg';
+import Barista2 from '../assets/images/about/desktop/image-commitment.jpg';
+import Quality0 from  '../assets/images/about/mobile/image-quality.jpg'
+import Quality1 from  '../assets/images/about/tablet/image-quality.jpg'
+import Quality2 from  '../assets/images/about/desktop/image-quality.jpg'
+import HeroWhitecup0 from '../assets/images/about/mobile/image-hero-whitecup.jpg';
+import HeroWhitecup1 from '../assets/images/about/tablet/image-hero-whitecup.jpg';
+import HeroWhitecup2 from '../assets/images/about/desktop/image-hero-whitecup.jpg';
+import CoffeeHero0 from '../assets/images/home/mobile/image-hero-coffeepress.jpg';
+import CoffeeHero1 from '../assets/images/home/tablet/image-hero-coffeepress.jpg';
+import CoffeeHero2 from '../assets/images/home/desktop/image-hero-coffeepress.jpg';
+import HeroBlackcup0 from '../assets/images/plan/mobile/image-hero-blackcup.jpg';
+import HeroBlackcup1 from '../assets/images/plan/tablet/image-hero-blackcup.jpg';
+import HeroBlackcup2 from '../assets/images/plan/desktop/image-hero-blackcup.jpg';
+
+
+const Imagens = {
+   CoffeeImage0: CoffeeImage0,
+   CoffeeImage1: CoffeeImage1,
+   CoffeeImage3: CoffeeImage3,
+   CoffeeImage4: CoffeeImage0,
+   LogoW: LogoWhite,
+   Icon0 : Icon0 ,
+   Icon1: Icon1,
+   Icon2: Icon2,
+   Logo: Logo,
+   Open: Open,
+   Close: Close,
+   Beans: Beans,
+   Gift: Gift,
+   Truck: Truck,
+   Barista0: Barista0,
+   Barista1: Barista1,
+   Barista2: Barista2,
+   Quality0 : Quality0 ,
+   Quality1: Quality1,
+   Quality2: Quality2,
+   HeroWhitecup0 : HeroWhitecup0,
+   HeroWhitecup1: HeroWhitecup1,
+   HeroWhitecup2: HeroWhitecup2,
+   CoffeeHero0 : CoffeeHero0 ,
+   CoffeeHero1: CoffeeHero1,
+   CoffeeHero2: CoffeeHero2,
+   HeroBlackcup0: HeroBlackcup0,
+   HeroBlackcup1: HeroBlackcup1,
+   HeroBlackcup2: HeroBlackcup2
+}
